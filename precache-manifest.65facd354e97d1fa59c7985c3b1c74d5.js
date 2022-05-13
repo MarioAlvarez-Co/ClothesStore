351fddcf2bbe28289022064233deb4d66f064682
Mario <lushoalvarez45@gmail.com>
2022-05-12 19:39:34 -0500

    Updates

---
+++ precache-manifest.65facd354e97d1fa59c7985c3b1c74d5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "14a9c417b964634ecf19",
+    "url": "/ClothesStore/static/css/main.f04bb3de.chunk.css"
+  },
+  {
+    "revision": "14a9c417b964634ecf19",
+    "url": "/ClothesStore/static/js/main.14a9c417.chunk.js"
+  },
+  {
+    "revision": "4c01fddca6c1bc376571",
+    "url": "/ClothesStore/static/js/1.4c01fddc.chunk.js"
+  },
+  {
+    "revision": "ad2bdb379a6d855c99ea",
+    "url": "/ClothesStore/static/js/runtime~main.ad2bdb37.js"
+  },
+  {
+    "revision": "ddf3587a009d4858c51680c9866c8630",
+    "url": "/ClothesStore/static/media/banner-principal-desktop.ddf3587a.png"
+  },
+  {
+    "revision": "30cc51bb94940df71c20ab4051dfee41",
+    "url": "/ClothesStore/static/media/pexels-lumn-322207.30cc51bb.jpg"
+  },
+  {
+    "revision": "e3141b2a8c5b9d1bd800c873098c14b1",
+    "url": "/ClothesStore/static/media/facebook.e3141b2a.svg"
+  },
+  {
+    "revision": "a2247cc533870460471cd5c1a54392ee",
+    "url": "/ClothesStore/static/media/twitter.a2247cc5.svg"
+  },
+  {
+    "revision": "35affc8f40e8736154327cede8777114",
+    "url": "/ClothesStore/static/media/instagram.35affc8f.svg"
+  },
+  {
+    "revision": "35f80f67215d60b5b18182e5e87aa3de",
+    "url": "/ClothesStore/static/media/youtube.35f80f67.svg"
+  },
+  {
+    "revision": "f44b92a533c4843e6c67c972e56aef1a",
+    "url": "/ClothesStore/static/media/categorias-destacadas-moda-infantil.f44b92a5.png"
+  },
+  {
+    "revision": "95f8b79b84d3c845756593adba8a9ce4",
+    "url": "/ClothesStore/static/media/categorias-destacadas-proteccion.95f8b79b.png"
+  },
+  {
+    "revision": "e246d0507290c715bd5601d54d647f70",
+    "url": "/ClothesStore/index.html"
+  }
+];
